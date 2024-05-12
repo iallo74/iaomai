84d7bfaa76a62c06f7807337ae93fb2ef86d2e55
Christian Iallonardi <c.iallo@hotmail.it>
2024-05-13 00:01:16 +0200

    Aggiunto limite dimensioni file Video

--- js/interfaccia/modulo_photo.js
+++ js/interfaccia/modulo_photo.js
@@ -47,6 +47,7 @@ var PH = {
 							"video/x-ms-asf",
 							"video/quicktime" ],
 	maxFileSize: 20*1000*1000, // 20MB
+	maxVideoSize: 50*1000*1000, // 50MB
 	
 	encodeImageFileAsURL: function( element, resizable=false, makeBig=false, functPH='', listaEstensioni ) { // trasforma l'immagine in base64
 		if(typeof(listaEstensioni) == 'undefined'){
@@ -72,6 +73,10 @@ var PH = {
 		}
 		if(file.size > PH.maxFileSize && PH.listaEstensioniFiles.indexOf(file.type)>-1){
 			ALERT(TXT("DimensioneNonConsentita").replace("[maxSize]",PH.maxFileSize*.000001).replace("[size]",ArrotondaEuro(file.size*.000001)));
+			return;
+		}
+		if(file.size > PH.maxFileSize && PH.listaEstensioniVideo.indexOf(file.type)>-1){
+			ALERT(TXT("DimensioneNonConsentita").replace("[maxSize]",PH.maxVideoSize*.000001).replace("[size]",ArrotondaEuro(file.size*.000001)));
 			return;
 		}
 		let reader = new FileReader();
