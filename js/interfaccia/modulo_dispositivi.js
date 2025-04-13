618dc5a282f06d071b614d50f559d0bb740ac573
Christian Iallonardi <c.iallo@hotmail.it>
2025-04-13 17:12:53 +0200

    Restyling schermata dispositivi a seguito parametrizzazione dispositivi connessi

--- js/interfaccia/modulo_dispositivi.js
+++ js/interfaccia/modulo_dispositivi.js
@@ -2,50 +2,30 @@ var DISPOSITIVI = {
 
 	carica: function( txt ){
 		let HTML = '<div id="dispositivi_descr">'+htmlEntities(TXT("MaxDispositiviConnessi"))+'</div>',
-			elenco = JSON.parse(txt),
-			devs1 = devs2 = type1 = type2 = time1 = time2 = plt1 = plt2 = '';
-		//console.log(elenco)
-		if(elenco.DeviceInfo)devs1 = JSON.parse(window.atob(elenco.DeviceInfo));
-		if(elenco.DeviceInfo2)devs2 = JSON.parse(window.atob(elenco.DeviceInfo2));
-		if(parseInt(elenco.LastConn))time1 = "<br><i>"+TXT("UltimaConnessione")+"</i>: "+getDataTS(elenco.LastConn);
-		if(parseInt(elenco.LastConn2))time2 = "<br><i>"+TXT("UltimaConnessione")+"</i>: "+getDataTS(elenco.LastConn2);
-		
-		if(devs1.appType=='APP')type1 = 'APP';
-		else if(devs1.browser)type1 = 'Online: <img src="img/ico_browser_'+devs1.browser.toLowerCase()+'.png"> '+devs1.browser;
-		else type1 = 'WEB APP';
-		if(devs2.appType=='APP')type2 = 'APP';
-		else if(devs2.browser)type2 = 'Online: <img src="img/ico_browser_'+devs2.browser.toLowerCase()+'.png"> '+devs2.browser;
-		else type2 = 'WEB APP';
-		if(devs1.platform){
-			if(devs1.platform.toLowerCase().replace(" ","").indexOf("android")>-1)plt1 = 'android';
-			if(devs1.platform.toLowerCase().replace(" ","").indexOf("macos")>-1)plt1 = 'macos';
-			if(devs1.platform.toLowerCase().replace(" ","").indexOf("macintosh")>-1)plt1 = 'macos';
-			if(devs1.platform.toLowerCase().replace(" ","").indexOf("windows")>-1)plt1 = 'windows';
-			if(devs1.platform.toLowerCase().replace(" ","").indexOf("ios")>-1)plt1 = 'macos';//'ios';
-			if(devs1.platform.toLowerCase().replace(" ","").indexOf("linux")>-1)plt1 = 'linux';
-		}
-		if(devs2.platform){
-			if(devs2.platform.toLowerCase().replace(" ","").indexOf("android")>-1)plt2 = 'android';
-			if(devs2.platform.toLowerCase().replace(" ","").indexOf("macos")>-1)plt2 = 'macos';
-			if(devs2.platform.toLowerCase().replace(" ","").indexOf("macintosh")>-1)plt2 = 'macos';
-			if(devs2.platform.toLowerCase().replace(" ","").indexOf("windows")>-1)plt2 = 'windows';
-			if(devs2.platform.toLowerCase().replace(" ","").indexOf("ios")>-1)plt2 = 'macos';//'ios';
-			if(devs2.platform.toLowerCase().replace(" ","").indexOf("linux")>-1)plt2 = 'linux';
-		}
-		
-		HTML += '<div class="elsDispositivi'+ ((devs1.type.toLowerCase()!='computer') ? ' dispMobile"' : '') +'"' +
-				'	  style="background-image:url(img/ico_os_'+plt1+'.png),url(img/ico_'+devs1.type.toLowerCase()+'.png);">' +
+			elenco = JSON.parse(txt);
+		for(c in elenco.connections){
+			let EL = elenco.connections[c],
+				devs = type = time = plt = '';
+			if(EL.DeviceInfo)devs = JSON.parse(window.atob(EL.DeviceInfo));
+			if(parseInt(EL.LastConn))time1 = "<br><i>"+TXT("UltimaConnessione")+"</i>: "+getDataTS(EL.LastConn);
+			if(devs.appType=='APP')type = 'APP';
+			else if(devs.browser)type = 'Online: <img src="img/ico_browser_'+devs.browser.toLowerCase()+'.png"> '+devs.browser;
+			else type = 'WEB APP';
+			if(devs.platform){
+				if(devs.platform.toLowerCase().replace(" ","").indexOf("android")>-1)plt = 'android';
+				if(devs.platform.toLowerCase().replace(" ","").indexOf("macos")>-1)plt = 'macos';
+				if(devs.platform.toLowerCase().replace(" ","").indexOf("macintosh")>-1)plt = 'macos';
+				if(devs.platform.toLowerCase().replace(" ","").indexOf("windows")>-1)plt = 'windows';
+				if(devs.platform.toLowerCase().replace(" ","").indexOf("ios")>-1)plt = 'macos';//'ios';
+				if(devs.platform.toLowerCase().replace(" ","").indexOf("linux")>-1)plt = 'linux';
+			}
+			HTML += '<div class="elsDispositivi'+ ((devs.type.toLowerCase()!='computer') ? ' dispMobile"' : '') +'"' +
+				'	  style="background-image:url(img/ico_os_'+plt+'.png),url(img/ico_'+devs.type.toLowerCase()+'.png);">' +
 				'	<span class="btn_invia btn_logout_mini"' +
-				'	 	   onClick="DISPOSITIVI.disconnetti(\''+elenco.UniqueId+'\');"></span>' +
-					devs1.type + " " + devs1.platform + "<br>" + type1 + " " + time1 +
-				'</div>' +
-				'<div class="elsDispositivi'+ ((devs2.type.toLowerCase()!='computer') ? ' dispMobile"' : '') +'"' +
-				'	  style="background-image:url(img/ico_os_'+plt2+'.png),url(img/ico_'+devs2.type.toLowerCase()+'.png);">' +
-				'	<span class="btn_invia btn_logout_mini"' +
-				'	 	   onClick="DISPOSITIVI.disconnetti(\''+elenco.UniqueId2+'\');"></span>' +
-					devs2.type + " " + devs2.platform + "<br>" + type2 + " " + time2 +
+				'	 	   onClick="DISPOSITIVI.disconnetti(\''+EL.UniqueId+'\');"></span>' +
+					devs.type + " " + devs.platform + "<br>" + type + " " + time +
 				'</div>';
-			
+		}
 		document.getElementById("contDispositivi").innerHTML = HTML;
 	},
 	
